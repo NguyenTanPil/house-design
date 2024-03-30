2ca06b648a643c45d349e851a9083b9c21674e09
NguyenTanPil <traila1008@gmail.com>
2024-03-30 13:22:36 +0700

    Text overflow for title

--- src/dummy.js
+++ src/dummy.js
@@ -60,3 +60,118 @@ export const newArchitecture = [
 		imgUrl: 'https://decoxdesign.com/upload/sanpham/the-silver-townhouse.jpg',
 	},
 ];
+export const interiorDesign = [
+	{
+		id: 'a1',
+		title: 'Deluxe Townhouse',
+		desc: 'New Architecture',
+		imgUrl: 'https://decoxdesign.com/upload/sanpham/dk-villa.jpg',
+	},
+	{
+		id: 'a2',
+		title: 'Deluxe Townhouse',
+		desc: 'New Architecture',
+		imgUrl: 'https://decoxdesign.com/upload/sanpham/the-silver-townhouse.jpg',
+	},
+	{
+		id: 'a3',
+		title: 'Deluxe Townhouse',
+		desc: 'New Architecture',
+		imgUrl: 'https://decoxdesign.com/upload/sanpham/bamboo-villa.jpg',
+	},
+	{
+		id: 'a4',
+		title: 'Deluxe Townhouse',
+		desc: 'New Architecture',
+		imgUrl: 'https://decoxdesign.com/upload/sanpham/villa-del-sol.jpg',
+	},
+	{
+		id: 'a5',
+		title: 'Deluxe Townhouse',
+		desc: 'New Architecture',
+		imgUrl: 'https://decoxdesign.com/upload/sanpham/dk-villa.jpg',
+	},
+	{
+		id: 'a6',
+		title: 'Deluxe Townhouse',
+		desc: 'New Architecture',
+		imgUrl: 'https://decoxdesign.com/upload/sanpham/the-indofuture-sky-villa.jpg',
+	},
+	{
+		id: 'a7',
+		title: 'Deluxe Townhouse',
+		desc: 'New Architecture',
+		imgUrl: 'https://decoxdesign.com/upload/sanpham/annam-villa.jpg',
+	},
+	{
+		id: 'a8',
+		title: 'Deluxe Townhouse',
+		desc: 'New Architecture',
+		imgUrl: 'https://decoxdesign.com/upload/sanpham/the-deluxe-townhouse.jpg',
+	},
+	{
+		id: 'a9',
+		title: 'Deluxe Townhouse',
+		desc: 'New Architecture',
+		imgUrl: 'https://decoxdesign.com/upload/sanpham/deluxe-townhouse.jpg',
+	},
+	{
+		id: 'a10',
+		title: 'Deluxe Townhouse',
+		desc: 'New Architecture',
+		imgUrl: 'https://decoxdesign.com/upload/sanpham/the-silver-townhouse.jpg',
+	},
+];
+export const tiktokStories = [
+	{
+		id: 'a1',
+		title: 'Giải mã màu trắng - Gam màu Quốc dân',
+		imgUrl: 'https://decoxdesign.com/upload/tiktok/giai-ma-mau-trang-gam-mau-quoc-dan.jpg',
+	},
+	{
+		id: 'a2',
+		title: '3 Lỗi chiếu sáng phổ biến',
+		imgUrl: 'https://decoxdesign.com/upload/tiktok/3-loi-chieu-sang-pho-bien.jpg',
+	},
+	{
+		id: 'a3',
+		title: 'Modern Luxury sẽ không bao giờ lỗi thời?',
+		imgUrl: 'https://decoxdesign.com/upload/tiktok/modern-luxury-se-khong-bao-gio-loi-thoi.jpg',
+	},
+	{
+		id: 'a4',
+		title: 'Làm Kiến trúc sư có khó không?',
+		imgUrl: 'https://decoxdesign.com/upload/tiktok/modern-luxury-se-khong-bao-gio-loi-thoi.jpg',
+	},
+	{
+		id: 'a5',
+		title: 'Bố trí cửa sao cho đúng phong thuỷ?',
+		imgUrl: 'https://decoxdesign.com/upload/tiktok/bo-tri-cua-sao-cho-dung-phong-thuy-chuyen-phong-thuy.jpg',
+	},
+	{
+		id: 'a6',
+		title: 'Bố trí cửa sao cho đúng phong thuỷ?',
+		imgUrl: 'https://decoxdesign.com/upload/sanpham/the-indofuture-sky-villa.jpg',
+	},
+	{
+		id: 'a7',
+		title: 'Không gian cho trẻ em, Cảm hứng bắt nguồn từ đâu?',
+		imgUrl: 'https://decoxdesign.com/upload/tiktok/khong-gian-cho-tre-em-cam-hung-bat-nguon-tu-dau.jpg',
+	},
+	{
+		id: 'a8',
+		title: 'The Elegant Villa - Dự án tại Thạnh Mỹ Lợi Quận 2',
+		imgUrl:
+			'https://decoxdesign.com/upload/tiktok/the-elegant-villa-du-an-design-and-build-by-decox-tai-thanh-my-loi-quan-2.jpg',
+	},
+	{
+		id: 'a9',
+		title: 'Chọn Cafe nào để tiếp khách | Hôm nay Decox chọn Runam',
+		imgUrl: 'https://decoxdesign.com/upload/tiktok/chon-cafe-nao-de-tiep-khach-hom-nay-decox-chon-runam.jpg',
+	},
+	{
+		id: 'a10',
+		title: 'The Yacht House - Khám phá du thuyền trên mặt đất',
+		imgUrl: 'https://decoxdesign.com/upload/tiktok/the-yacht-house-kham-pha-du-thuyen-tren-mat-dat.jpg',
+	},
+];
