6b11086be4b5b531869ce4d10fafe30145888f9c
NguyenTanPil <traila1008@gmail.com>
2024-03-30 16:49:21 +0700

    Add container for breakcrumb

--- src/components/Breadcrumbs/index.jsx
+++ src/components/Breadcrumbs/index.jsx
@@ -1,60 +1,63 @@
-import Breadcrumbs from "@mui/material/Breadcrumbs";
-import Link from "@mui/material/Link";
-import HomeIcon from "@mui/icons-material/Home";
-import { useParams } from "react-router-dom";
-import { Typography } from "@mui/material";
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import Link from '@mui/material/Link';
+import HomeIcon from '@mui/icons-material/Home';
+import { useParams } from 'react-router-dom';
+import { Container, Typography } from '@mui/material';
 
 const getTextTranslateBreadcrumb = (str) => {
-    switch (str) {
-        case "mau-nha-dep":
-            return "Mẫu Nhà Đẹp";
-        case "phong-khach":
-            return "Phòng Khách";
-            case "phong-bep":
-                return 'Phòng Bếp';
-            case "phong-ngu":
-                return 'Phòng Ngủ';
-            case "ban-cong":
-                return 'Ban Công';
-        default:
-            return "";
-    }
+	switch (str) {
+		case 'mau-nha-dep':
+			return 'Mẫu Nhà Đẹp';
+		case 'phong-khach':
+			return 'Phòng Khách';
+		case 'phong-bep':
+			return 'Phòng Bếp';
+		case 'phong-ngu':
+			return 'Phòng Ngủ';
+		case 'ban-cong':
+			return 'Ban Công';
+		default:
+			return '';
+	}
 };
 
 export default function Breadcrumb() {
-    const { category } = useParams();
+	const { category } = useParams();
 
-    return (
-        <div
-            style={{
-                backgroundColor: "#f7f7f7",
-                width: "60%",
-                padding: "8px 0px 8px 4px",
-                border: "1px solid #ececec",
-                boxSizing: "content-box",
-            }}
-        >
-            <div role="presentation">
-                <Breadcrumbs aria-label="breadcrumb">
-                    <Link
-                        underline="hover"
-                        sx={{
-                            display: "flex",
-                            alignItems: "center",
-                            color: "#000",
-                        }}
-                        href="/"
-                    >
-                        <HomeIcon sx={{ mr: 0.5 }} fontSize="inherit" />
-                        Home
-                    </Link>
-                    <Typography
-                        sx={{ display: "flex", alignItems: "center" }}
-                        color="text.primary"
-                    >
-                        {getTextTranslateBreadcrumb(category)}
-                    </Typography>
-                    {/* <Link
+	return (
+		<Container>
+			<div
+				style={{
+					backgroundColor: '#f7f7f7',
+					padding: '8px 0px 8px 4px',
+					border: '1px solid #ececec',
+					boxSizing: 'content-box',
+				}}
+			>
+				<div role='presentation'>
+					<Breadcrumbs aria-label='breadcrumb'>
+						<Link
+							underline='hover'
+							sx={{
+								display: 'flex',
+								alignItems: 'center',
+								color: '#000',
+							}}
+							href='/'
+						>
+							<HomeIcon
+								sx={{ mr: 0.5 }}
+								fontSize='inherit'
+							/>
+							Home
+						</Link>
+						<Typography
+							sx={{ display: 'flex', alignItems: 'center' }}
+							color='text.primary'
+						>
+							{getTextTranslateBreadcrumb(category)}
+						</Typography>
+						{/* <Link
           underline="hover"
           sx={{ display: 'flex', alignItems: 'center' }}
           color="inherit"
@@ -62,8 +65,9 @@ export default function Breadcrumb() {
         >
           {category}
         </Link> */}
-                </Breadcrumbs>
-            </div>
-        </div>
-    );
+					</Breadcrumbs>
+				</div>
+			</div>
+		</Container>
+	);
 }
